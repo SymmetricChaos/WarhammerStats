02d6b34a4da812829b4bf20ba4230e6664e4ce3b
SymmetricChaos <ajfraebel@gmail.com>
2020-06-01 22:06:08 -0400

    Update JSONtoDataFrame.py

--- JSONtoDataFrame.py
+++ JSONtoDataFrame.py
@@ -26,10 +26,42 @@ def show_attributes(D):
     for line in att:
         print(line["key"])
         print()
+        
+def show_factions(D):
+    fac = D["factions"]
+    for line in fac:
+        print(line["screen_name"])
 
+def get_faction_group(D):
+    return D["key"].split("_")[2]
+    
 
-unit = J[5]
+#unit = J[15]
 #show_dict(unit)
 #show_primary_melee_weapon(unit)
 #show_primary_missile_weapon(unit)
-show_attributes(unit)
+#show_attributes(unit)
+#show_factions(unit)
+
+
+#castes = set([])
+#categories = set([])
+#special_categories = set([])
+#faction_groups = set([])
+#
+#for unit in J:
+#    castes.add(unit["caste"])
+#    categories.add(unit["category"])
+#    special_categories.add(unit["special_category"])
+#    faction_groups.add(get_faction_group(unit))
+#    
+#print(castes)
+#print(categories)
+#print(special_categories)
+#print(faction_groups)
+        
+        
+for unit in J:
+    if "neu" in unit["key"]:
+        print(unit["key"])
+        
