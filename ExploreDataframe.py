aee903112ea18578e16545336664f169e586b679
SymmetricChaos <ajfraebel@gmail.com>
2020-06-02 18:03:34 -0400

    quick exploring functions

--- ExploreDataframe.py
+++ ExploreDataframe.py
@@ -5,6 +5,23 @@ import numpy as np
 unitsDF = pickle.load( open( "unitsDF.p", "rb" ) )
 pd.set_option('display.max_rows', 500)
 
-r, = np.random.randint(0,1365,1)
+def random_unit():
+    # randint returns a list, the comma just skips having to extract the only
+    # element of the list
+    r, = np.random.randint(0,1365,1)
+    print(unitsDF.iloc[r])
 
-print(unitsDF.iloc[r])
+def show_categorical_stats():
+    for cat in ['caste','category','entity_size','faction_group','entity_size',
+                'ground_stat_effect_group','special_category']:
+        
+        options = sorted(unitsDF[cat].unique())
+        print(f"{cat}:\n{options}\n")
+
+
+if __name__ == '__main__':
+    
+    print("Show A random Unit's Stats")
+    random_unit()
+    print("\n\n\nShow the various categories a unit can fall into")
+    show_categorical_stats()
