03a330451cd3b0f9217b4802a889edd4e8bedf8f
SymmetricChaos <ajfraebel@gmail.com>
2020-06-03 08:22:58 -0400

    Update WarhammerArmor.py

--- WarhammerArmor.py
+++ WarhammerArmor.py
@@ -4,7 +4,7 @@ from UtilityFunctions import average_damage_with_armor_ratio
 
 
 
-def armor_graph(max_armor=200,ap_fractions=[0,.2,.4,.6,.8]):
+def armor_graph(max_armor=200,ap_fractions=[.1,.2,.4,.6,.8]):
     
     legend_names = [f"{int(i*100)}% AP" for i in ap_fractions]
     armor = np.linspace(0,max_armor,200)
@@ -17,7 +17,8 @@ def armor_graph(max_armor=200,ap_fractions=[0,.2,.4,.6,.8]):
     for ap in ap_fractions:
         hp_mult = [100/average_damage_with_armor_ratio(100,ap,a) for a in armor]
         plt.plot(armor,hp_mult)
-        plt.annotate(f"{round(hp_mult[-1],1)}",(max_armor+2,hp_mult[-1]))
+        if hp_mult[-1] < 11:
+            plt.annotate(f"{round(hp_mult[-1],1)}",(max_armor+1.1,hp_mult[-1]))
         upper_lims.append(hp_mult[-1])
         
     plt.xlabel("Armor")
@@ -27,12 +28,12 @@ def armor_graph(max_armor=200,ap_fractions=[0,.2,.4,.6,.8]):
     plt.legend(legend_names)
     plt.xticks(x_ticks)
     
-    plt.ylim([.5,min(max(upper_lims)+1,11)])
-    plt.xlim([-5,max_armor+5])
+    plt.ylim([1-max_armor/400,min(max(upper_lims)+1,11)])
+    plt.xlim([-max_armor/20,max_armor+max_armor/20])
     plt.title(f"HP Multiplier from Armor (0 to {max_armor})",size=20)
     plt.show()
 
 
 
 if __name__ == '__main__':
-    armor_graph(120)
+    armor_graph(150)
