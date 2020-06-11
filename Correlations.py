43bdaf3f00dc6e20181132872e6211e554ce4f44
SymmetricChaos <ajfraebel@gmail.com>
2020-06-11 11:38:56 -0400

    Update Correlations.py

--- Correlations.py
+++ Correlations.py
@@ -7,22 +7,31 @@ from sklearn.metrics import mean_squared_error, r2_score, median_absolute_error
 
 unitsDF = pickle.load( open( "unitsDF.p", "rb" ) )
 
-melee_attack = unitsDF["melee_attack"].values.reshape(-1,1)
-melee_defence = unitsDF["melee_defence"].values.reshape(-1,1)
 
 
-model = linear_model.LinearRegression() 
-model.fit(melee_attack,melee_defence)
+# Simple linear correlation take from a unitsDF dataframe
+def show_correlation(X,Y):
+    Xname = " ".join(X.name.split("_"))
+    Yname = " ".join(X.name.split("_"))
+    X = X.values.reshape(-1,1)
+    Y = Y.values.reshape(-1,1)
+    
+    
+    model = linear_model.LinearRegression() 
+    model.fit(X,Y)
+    
+    predicted_Y = model.predict(X)
+    
+    plt.scatter(X,Y)
+    plt.xlabel(Xname.title())
+    plt.ylabel(Yname.title())
+    plt.plot(X,predicted_Y,
+                color='red')
+    
+    R2 = r2_score(X, predicted_Y)
+    MAD = median_absolute_error(X, predicted_Y)
+    print(f"R-squared: {round(R2,2)}")
+    print(f"Median Absolute Error: {round(MAD,2)}")
 
-predicted_melee_defence = model.predict(melee_attack)
 
-plt.scatter(melee_attack,melee_defence)
-plt.xlabel("Melee Attack")
-plt.ylabel("Melee Defence")
-plt.plot(melee_attack,predicted_melee_defence,
-            color='red')
-
-R2 = r2_score(melee_defence, predicted_melee_defence)
-MAD = median_absolute_error(melee_defence, predicted_melee_defence)
-print(f"R-squared: {round(R2,2)}")
-print(f"Median Absolute Error: {round(MAD,2)}")
+show_correlation(unitsDF["melee_attack"],unitsDF["melee_defence"])
