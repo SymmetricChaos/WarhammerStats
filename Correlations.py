8391360db7659df355a4e9678515daf25c4809ae
SymmetricChaos <ajfraebel@gmail.com>
2020-06-11 08:49:18 -0400

    Update Correlations.py

--- Correlations.py
+++ Correlations.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pickle
 import pandas as pd
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, median_absolute_error
 
 unitsDF = pickle.load( open( "unitsDF.p", "rb" ) )
 
@@ -21,3 +21,8 @@ plt.xlabel("Melee Attack")
 plt.ylabel("Melee Defence")
 plt.plot(melee_attack,predicted_melee_defence,
             color='red')
+
+R2 = r2_score(melee_defence, predicted_melee_defence)
+MAD = median_absolute_error(melee_defence, predicted_melee_defence)
+print(f"R-squared: {round(R2,2)}")
+print(f"Median Absolute Error: {round(MAD,2)}")
