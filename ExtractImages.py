e11f6d1316de279104e1d2b920f846a196ceb4ff
SymmetricChaos <ajfraebel@gmail.com>
2020-06-13 12:34:48 -0400

    File for getting images

---
+++ ExtractImages.py
@@ -0,0 +1,22 @@
+# The UI file is extracted from the game using the File Pack Manager
+
+import os
+import shutil
+cur_dir = os.getcwd()
+flags = cur_dir+"\\ui\\flags"
+
+faction_names = []
+
+for r,d,f in os.walk(flags):
+    if f == []:
+        continue
+#    # Horrible messy 
+    faction_name = "_".join(r[len(flags)+5:].split("_")[1:])
+    
+    for size in ["24","64","256"]:
+        try:
+            file24 = f"{r}\\mon_{size}.png"
+            newfile24 = f"{cur_dir}\\FactionFlags\\{faction_name}_{size}.png"
+            shutil.copyfile(file24,newfile24)
+        except:
+            print(f"{faction_name}_{size} not found")
