ee321537db2e449066e2621fa799b961e7d4842e
SymmetricChaos <ajfraebel@gmail.com>
2020-07-13 08:01:49 -0400

    Update UnitClassTest.py

--- UnitClassTest.py
+++ UnitClassTest.py
@@ -1,24 +1,19 @@
 import pickle
 from UtilityFunctions import select_unit
 from TWWObjects import TWWUnit
-effects_dict = pickle.load( open( "effectsDict.p", "rb" ) )
 
+effects_dict = pickle.load( open( "effectsDict.p", "rb" ) )
 unitsDF = pickle.load( open( "unitsDF.p", "rb" ) )
 
 my_unit = TWWUnit(select_unit(unitsDF,"Phoenix Guard"))
 
-print(my_unit)
-
 print("\nTest Bonus Vs Large")
-print("Currently it is off")
 my_unit.unit_card()
 
 my_unit.toggle_BvL()
-print("Toggled to on")
 my_unit.unit_card()
 
 my_unit.toggle_BvL()
-print("Toggled back off")
 my_unit.unit_card()
 
 
@@ -52,5 +47,9 @@ my_unit.toggle_effect(effects_dict["Stand Your Ground!"])
 my_unit.unit_card()
 
 
+
+
+
+print("\n\n\nExample Ranged Unit")
 sisters = TWWUnit(select_unit(unitsDF,"Sisters of Avelorn"))
 sisters.unit_card()
